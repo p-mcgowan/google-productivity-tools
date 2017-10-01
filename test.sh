b6728493a279f55f84a64a651c518e33f9de432e
p-mcgowan <mickeymcgowan@shaw.ca>
2017-10-01 14:28:04 -0700

    test

--- test.sh
+++ test.sh
@@ -1,7 +1,27 @@
-echo "./google -oob '--option-one' '--option-two' mail -nb gmail"
-echo "shoudlnt say: Could not find bookmark ''"
-./google -oob '--option-one' '--option-two' mail -nb gmail
+expect() {
+    echo "expect:"
+    args=("$@")
+    for ((i = 0; i < ${#args[@]}; i++)); do
+        echo "  google-chrome ${args[$i]}"
+    done
+}
+runnable="./google -oob '--option-one' '--option-two' mail -nb gmail"
+echo $runnable
+expect "--option-one --option-two bookmarkmail" "--new-window bookmarkgmail"
+echo Result:
+$runnable
+echo
 
-echo "./google -oob '--option-one' '--option-two' mail -n -b gmail"
-echo "doesnt screw up finding bookmark"
-./google -oob '--option-one' '--option-two' mail -n -b gmail
+runnable="./google -oob '--option-one' '--option-two' mail -n -b gmail"
+echo $runnable
+expect "--option-one --option-two bookmarkmail" "--new-window bookmarkgmail"
+echo Result:
+$runnable
+echo
+
+runnable="./google -n -b gmail"
+echo $runnable
+expect "--new-window bookmarkgmail"
+echo Result:
+$runnable
+echo
