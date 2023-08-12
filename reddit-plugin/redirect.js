1666a2363a5b76bb4e19a91c5b9a1de83398c5dc
p-mcgowan@winbot <winbot@patmcgowan.dev>
2023-08-12 08:21:44 +0200

    allow new reddit media url

--- reddit-plugin/redirect.js
+++ reddit-plugin/redirect.js
@@ -2,5 +2,9 @@ chrome.storage.sync.get(['enabled'], (items) => {
   if (!items.enabled) {
     return;
   }
+  // allow i.reddit redirects
+  if (location.pathname.startsWith('/media')) {
+    return;
+  }
   location.replace(location.href.replace(/https:\/\/(www|new)\.reddit/, 'https://old.reddit'));
 });
