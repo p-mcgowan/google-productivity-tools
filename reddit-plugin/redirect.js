c83f23597f8b25288e94529b72d2cdbbea01bf54
p-mcgowan@winbot <winbot@patmcgowan.dev>
2023-08-19 10:30:28 +0200

    reddit-plugin: allow new reddit gallery

--- reddit-plugin/redirect.js
+++ reddit-plugin/redirect.js
@@ -3,7 +3,7 @@ chrome.storage.sync.get(['enabled'], (items) => {
     return;
   }
   // allow i.reddit redirects
-  if (location.pathname.startsWith('/media')) {
+  if (location.pathname.startsWith('/media') || location.pathname.startsWith('/gallery')) {
     return;
   }
   location.replace(location.href.replace(/https:\/\/(www|new)\.reddit/, 'https://old.reddit'));
