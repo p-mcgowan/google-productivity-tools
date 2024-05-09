7c352f2c59fe2d602f01bcdd1109360535ebb39b
p-mcgowan <mcgowanapat+git@gmail.com>
2024-05-09 11:06:38 +0200

    reddit: disable poll redirect

--- reddit-plugin/redirect.js
+++ reddit-plugin/redirect.js
@@ -1,10 +1,18 @@
+const noRedirects = [
+  '/media',
+  '/gallery',
+  '/poll',
+];
+
 chrome.storage.sync.get(['enabled'], (items) => {
   if (!items.enabled) {
     return;
   }
   // allow i.reddit redirects
-  if (location.pathname.startsWith('/media') || location.pathname.startsWith('/gallery')) {
+
+  if (noRedirects.find(path => location.pathname.indexOf(path) === 0)) {
     return;
   }
+
   location.replace(location.href.replace(/https:\/\/(www|new)\.reddit/, 'https://old.reddit'));
 });
